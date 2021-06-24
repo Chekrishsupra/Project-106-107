634bf9bf57c0c084fd795b7517daee656e5ec02e
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-06-24 09:01:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+prediction_1 = "";
+prediction_2 = "";
+Webcam.set({
+    width: 340 ,
+    height: 300 ,
+    image_format: "png",
+    png_quality : 90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_uri+"'>"; 
+    });
+}
+console.log("Ml5 Version:",ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/r9Js5GzPDZ/model.json",ModelLoaded);
+function ModelLoaded() {
+    console.log("Model Loaded");
+}
+function speak() {
+   var synth = window.speechSynthesis;
+   speak_data_1 = "The first prediction is " + prediction_1;
+   speak_data_2 = "And the second prediction is" + prediction_2;
+   var Utterthis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+   synth.speak(Utterthis);
+}
